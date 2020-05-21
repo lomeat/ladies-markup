38aadbae44e25444683942562215e9cb4cf4634d
lomeat <lom3at@gmail.com>
2020-05-21 04:29:59 +0300

    new start

---
+++ rollup.config.js
@@ -0,0 +1,67 @@
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
+import sveltePreprocess from "svelte-preprocess";
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: "src/main.js",
+  output: {
+    sourcemap: true,
+    format: "iife",
+    name: "app",
+    file: "public/build/bundle.js",
+  },
+  plugins: [
+    svelte({
+      dev: !production,
+      css: (css) => {
+        css.write("public/build/bundle.css");
+      },
+      preprocess: sveltePreprocess({
+        scss: {
+          includePaths: ["src"],
+          data: `@import './style/_theme.scss';`,
+        },
+        postcss: {
+          plugins: [require("autoprefixer")],
+        },
+      }),
+    }),
+
+    resolve({
+      browser: true,
+      dedupe: ["svelte"],
+    }),
+    commonjs(),
+
+    !production && serve(),
+
+    !production && livereload("public"),
+
+    production && terser(),
+  ],
+  watch: {
+    clearScreen: false,
+  },
+};
+
+function serve() {
+  let started = false;
+
+  return {
+    writeBundle() {
+      if (!started) {
+        started = true;
+
+        require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
+          stdio: ["ignore", "inherit", "inherit"],
+          shell: true,
+        });
+      }
+    },
+  };
+}
---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let name;
+</script>
+
+<style lang="scss">
+  :global(*) {
+    box-sizing: border-box;
+    outline: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    user-select: none;
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
