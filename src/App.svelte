3bf126eba4af4e12701af26eeacee600d05fc3c8
lomeat <lom3at@gmail.com>
2020-05-21 05:16:24 +0300

    move header to component

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,11 @@
 <script>
-  import LogoIcon from "./icons/logo.svg";
-  import SearchIcon from "./icons/search.svg";
-  import CartIcon from "./icons/cart.svg";
-  import MenuIcon from "./icons/menu.svg";
-
   import Button from "./Button.svelte";
+  import Header from "./Header.svelte";
 </script>
 
 <div class="wrapper">
   <div class="back-blue-square" />
-  <header>
-    <div class="logo">
-      <span>Ladies</span>
-      <LogoIcon style="margin-left: 14px" width="26" />
-    </div>
-
-    <div class="header-other">
-      <div class="search">
-        <input placeholder="Search" />
-        <div class="search-button">
-          <Button>
-            <SearchIcon width="20" />
-          </Button>
-        </div>
-      </div>
-      <div class="cart">
-        <Button>
-          <CartIcon width="28" />
-        </Button>
-      </div>
-      <div>
-        <Button class="menu">
-          <MenuIcon width="20" />
-        </Button>
-      </div>
-    </div>
-  </header>
+  <Header />
   <main>
     <div class="best-sellers" />
     <div class="accessories" />
---
+++ src/Header.svelte
@@ -0,0 +1,41 @@
+<script>
+  import LogoIcon from "./icons/logo.svg";
+  import SearchIcon from "./icons/search.svg";
+  import CartIcon from "./icons/cart.svg";
+  import MenuIcon from "./icons/menu.svg";
+
+  import Button from "./Button.svelte";
+</script>
+
+<header>
+  <div class="logo">
+    <span>Ladies</span>
+    <LogoIcon style="margin-left: 14px" width="26" />
+  </div>
+
+  <div class="header-other">
+    <div class="search">
+      <input placeholder="Search" />
+      <div class="search-button">
+        <Button>
+          <SearchIcon width="20" />
+        </Button>
+      </div>
+    </div>
+    <div class="cart">
+      <Button>
+        <CartIcon width="28" />
+      </Button>
+    </div>
+    <div>
+      <Button class="menu">
+        <MenuIcon width="20" />
+      </Button>
+    </div>
+  </div>
+</header>
+
+
+<style lang="scss">
+  @import "./style/header";
+</style>
